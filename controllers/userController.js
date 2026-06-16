bae8bddcd6ffabb2d431118d825dc0556322c593
Pinky <pinkysingh.sde@gmail.com>
2026-06-16 14:29:07 +0530

    fixed cookies

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,14 +5,14 @@ import jwt from "jsonwebtoken"
 // generate a token 
 const generateToken = (id, role) => {
 
-        return jwt.sign({
-            id , role
-        }, 
+    return jwt.sign({
+        id, role
+    },
         process.env.JWT_SECERT,
         {
-            expiresIn : "7d"
+            expiresIn: "7d"
         }
-    
+
     )
 }
 
@@ -20,53 +20,53 @@ const generateToken = (id, role) => {
 // register new user
 
 export const registerUser = async (req, res) => {
-    
+
 
     try {
-        const {name, email, password, role} = req.body
+        const { name, email, password, role } = req.body
 
-    // check if user exist
-    const userExist = await User.findOne({email})
+        // check if user exist
+        const userExist = await User.findOne({ email })
 
-    if(userExist){
-        return res.status(400).json({
-            message : "User already exist",
-            success : false
-        })
-    }
-
-    if (!email || !password) {
-    return res.status(400).json({
-        success: false,
-        message: "Email and password are required"
-    })
-}
-
-    // create a new user
-
-    const user = await User.create({
-        name,
-        email,
-        password,
-        role : role || "PATIENT"
-    })
-
-    return res.status(201).json({
-        message : "User is created successfully",
-        data : {
-            id : user._id,
-            name : user.name,
-            // password : user.password,
-            email : user.email,
-            role : user.role
+        if (userExist) {
+            return res.status(400).json({
+                message: "User already exist",
+                success: false
+            })
         }
-        
-    })
+
+        if (!email || !password) {
+            return res.status(400).json({
+                success: false,
+                message: "Email and password are required"
+            })
+        }
+
+        // create a new user
+
+        const user = await User.create({
+            name,
+            email,
+            password,
+            role: role || "PATIENT"
+        })
+
+        return res.status(201).json({
+            message: "User is created successfully",
+            data: {
+                id: user._id,
+                name: user.name,
+                // password : user.password,
+                email: user.email,
+                role: user.role
+            }
+
+        })
 
     } catch (error) {
         res.status(500).json({
-            message : error.message,
-            success : false
+            message: error.message,
+            success: false
         })
     }
 
@@ -81,32 +81,32 @@ export const loginUser = async (req, res) => {
     try {
 
         // getting data
-        const {email, password} = req.body   
+        const { email, password } = req.body
 
-         // check user if it exist
-        const user = await User.findOne({email}).select("+password")
+        // check user if it exist
+        const user = await User.findOne({ email }).select("+password")
 
-        if(!user){
+        if (!user) {
             return res.status(401).json({
-                message : "Email or user is not found",
-                success : false
+                message: "Email or user is not found",
+                success: false
             })
         }
 
         if (!email || !password) {
-    return res.status(400).json({
-        success: false,
-        message: "Email and password are required"
-    })
-}
+            return res.status(400).json({
+                success: false,
+                message: "Email and password are required"
+            })
+        }
 
         // matching password - must call on user instance
         const isPasswordMatch = await user.matchpassword(password)
 
-        if(!isPasswordMatch){
+        if (!isPasswordMatch) {
             return res.status(401).json({
-                message : "Invalid email or password",
-                success : false
+                message: "Invalid email or password",
+                success: false
             })
         }
 
@@ -115,44 +115,44 @@ export const loginUser = async (req, res) => {
         const token = generateToken(user._id, user.role)
 
         res.cookie("token", token, {
-           httpOnly: true,
-           sameSite: "lax",
-           secure : false, 
-           maxAge : 7 * 24 * 60 * 60 * 1000
-        })
+            httpOnly: true,
+            secure: true,
+            sameSite: "none",
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+        });
 
         // Send success response
         res.status(200).json({
-            message : "Login successful",
-            success : true,
+            message: "Login successful",
+            success: true,
             // token : token,
-            data : {
-                id : user._id,
-                name : user.name,
-                email : user.email,
-                role : user.role
+            data: {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role
             }
         })
 
     } catch (error) {
         res.status(500).json({
-            message : error.message,
-            success : false
+            message: error.message,
+            success: false
         })
     }
- 
+
 }
 
 
-export const logoutUser = async(req, res) => {
-    res.clearCookie("token", "",  {
-    httpOnly: true,
-    sameSite: "lax",
-    secure: false
-})
+export const logoutUser = async (req, res) => {
+    res.clearCookie("token", "", {
+        httpOnly: true,
+        sameSite: "lax",
+        secure: false
+    })
     res.status(200).json({
-        message : "user is logout successfully",
-        success : true
+        message: "user is logout successfully",
+        success: true
     })
 
 }
