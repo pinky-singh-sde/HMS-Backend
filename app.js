05ec50038fab7f3764f2eef660b2b7987615c16b
Pinky <pinkysingh.sde@gmail.com>
2026-06-16 13:33:07 +0530

    fixes cors

--- app.js
+++ app.js
@@ -13,7 +13,10 @@ app.use(cookieParser())
 
 app.use(cors(
     {
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://hms-frontend-ebon.vercel.app/",
+    ],
     credentials: true
   }
 ))
